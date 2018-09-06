3ceffeb4e880992bd8cf13b01692a476a3b831a7
Martin Kamb <martiins@live.se>
2018-09-06 15:50:57 +0200

    Initial commit

---
+++ src/test/java/phptravels/SmokeTest.java
@@ -0,0 +1,12 @@
+package phptravels;
+
+import org.junit.jupiter.api.Test;
+
+public class SmokeTest extends FunctionalTest {
+
+    @Test
+    public void testDriver() {
+        Class1.go();
+    }
+
+}
